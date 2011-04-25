45cfa7c914ff25a295e09a57b4c42bca9642cb7f
sand8080 <sand8080@gmail.com>
2011-04-25 23:51:23 +0400

    Action logs generalization

--- src/helixweb/billing/urls.py
+++ src/helixweb/billing/urls.py
@@ -6,24 +6,7 @@ urlpatterns = patterns('',
 
     (r'^get_used_currencies/$', 'billing.views.used_currencies'),
     (r'^modify_used_currencies/$', 'billing.views.modify_used_currencies'),
-#
-#    (r'^user_info/(?P<id>\d+)/$', 'auth.views.user_info'),
-#    (r'^modify_users/(?P<id>\d+)/$', 'auth.views.modify_user'),
-#    (r'^get_action_logs/(?P<id>\d+)/$', 'auth.views.user_action_logs'),
-#    (r'^modify_user_self/$', 'auth.views.modify_user_self'),
-#    (r'^get_action_logs_self/$', 'auth.views.action_logs_self'),
-#
-#    (r'^get_services/$', 'auth.views.services'),
-#    (r'^add_service/$', 'auth.views.add_service'),
-#    (r'^modify_service/(?P<id>\d+)/$', 'auth.views.modify_service'),
-#
-#    (r'^get_groups/$', 'auth.views.groups'),
-#    (r'^add_group/$', 'auth.views.add_group'),
-#    (r'^delete_group/(?P<id>\d+)/$', 'auth.views.delete_group'),
-#    (r'^modify_group/(?P<id>\d+)/$', 'auth.views.modify_group'),
-#
-#    (r'^get_users/$', 'auth.views.users'),
-#    (r'^add_user/$', 'auth.views.add_user'),
-#
-#    (r'^get_action_logs/$', 'auth.views.action_logs'),
+
+    (r'^get_action_logs_self/$', 'billing.views.action_logs_self'),
+    (r'^get_action_logs/$', 'billing.views.action_logs'),
 )
