7cde9a62ca11b2c6350cabd67e446f1d27ab638e
Alexander Kislitsky <sand8080@gmail.com>
2011-05-14 22:38:49 +0400

    Current language handled in service switcher

--- src/helixweb/core/templatetags/service_switcher.py
+++ src/helixweb/core/templatetags/service_switcher.py
@@ -6,5 +6,5 @@ register = template.Library()
 
 @register.inclusion_tag('service_switcher.html', takes_context=True)
 def service_switcher(context):
-    return {'services': SUPPORTED_SERVICES,
-        'cur_service': context['cur_service']}
+    return {'services': SUPPORTED_SERVICES, 'cur_service': context['cur_service'],
+        'cur_lang': context['cur_lang']}
