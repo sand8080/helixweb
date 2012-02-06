24ff9a38832099a4f23e59f88181fb0295caaf4b
Alexander Kislitsky <sand8080@gmail.com>
2012-02-06 19:57:58 +0400

    Redirect to auth service from root added to urls

--- src/helixweb/urls.py
+++ src/helixweb/urls.py
@@ -1,7 +1,9 @@
-from django.conf.urls.defaults import * #@UnusedWildImport
+from django.conf.urls.defaults import include, patterns #@UnusedWildImport
+from django.views.generic.simple import redirect_to
 
 
 urlpatterns = patterns('',
+    (r'^$', redirect_to, {'url': '/auth/'}),
     (r'^auth/', include('helixweb.auth.urls')),
     (r'^billing/', include('helixweb.billing.urls')),
     (r'^tariff/', include('helixweb.tariff.urls')),
