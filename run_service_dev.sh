2220e36a349570f8abeb370c2456f7cf154c7a1a
Alexander Kislitsky <sand8080@gmail.com>
2012-11-20 22:15:44 +0400

    Development runscript added

---
+++ run_service_dev.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+BASENAME=`basename $0`
+CUR_DIR=`dirname $0`
+VENV_DIR=`readlink -f $CUR_DIR/.env`
+echo $VENV_DIR
+PYTHON_CMD="$VENV_DIR/bin/python"
+
+pushd "$CUR_DIR"
+export PYTHONPATH="$CUR_DIR/src:$CUR_DIR/../helixcore/src"
+DJANGO_CMD="$PYTHON_CMD $CUR_DIR/src/helixweb/manage.py runserver"
+echo $DJANGO_CMD
+$DJANGO_CMD
+popd
