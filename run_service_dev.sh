d96187a6a94f2965083d3a50c6a2d1e24411e374
Alexander Kislitsky <akislitsky@loyalty-partners.ru>
2012-12-06 16:06:42 +0400

    Dev server listening on all ips

--- run_service_dev.sh
+++ run_service_dev.sh
@@ -8,7 +8,8 @@ PYTHON_CMD="$VENV_DIR/bin/python"
 
 pushd "$CUR_DIR"
 export PYTHONPATH="$CUR_DIR/src:$CUR_DIR/../helixcore/src"
-DJANGO_CMD="$PYTHON_CMD $CUR_DIR/src/helixweb/manage.py runserver --settings=helixweb.settings_dev"
+#DJANGO_CMD="$PYTHON_CMD $CUR_DIR/src/helixweb/manage.py runserver --settings=helixweb.settings_dev"
+DJANGO_CMD="$PYTHON_CMD $CUR_DIR/src/helixweb/manage.py runserver 0.0.0.0:8000 --settings=helixweb.settings_dev"
 echo $DJANGO_CMD
 $DJANGO_CMD
 popd
