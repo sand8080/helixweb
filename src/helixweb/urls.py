6c0147b68e68ab06e68bd007fc8c1272852a689f
Alexander Kislitsky <akislitsky@loyalty-partners.ru>
2013-11-14 17:32:15 +0400

    Redirect fixed

--- src/helixweb/urls.py
+++ src/helixweb/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls.defaults import include, patterns #@UnusedWildImport
-from django.views.generic.simple import redirect_to
+#from django.views.generic.simple import redirect_to
+from django.views.generic import RedirectView
 
 
 urlpatterns = patterns('',
-    (r'^$', redirect_to, {'url': '/auth/'}),
+    (r'^$', RedirectView.as_view(url='/auth/')),
 
     (r'^auth/', include('helixweb.auth.urls')),
     (r'^billing/', include('helixweb.billing.urls')),
